d1bf2aa9be11193999cca67b893306477254425c
Christopher Auteri <christopher.auteri@icloud.com>
2017-11-15 17:02:09 -0500

    date change

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,10 @@
 window.onload = function () {
     var from = new Date();
-    var to = new Date(2018, 5, 1);
+    var to = new Date(2018, 5, 20);
     document.getElementById("current-year").innerHTML = "-"
         + from.getFullYear();
     document.getElementById("currently").innerHTML = '<small>'
         + Math.floor(Math.round(Math.max((to.getTime() - new Date(from.setHours(0, 0,
                     0, 0)).getTime())
                 / (24 * 60 * 60 * 1000), 0)) / 7) + '</small>';
-}
+};
