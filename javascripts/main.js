6299d013665ac78a81312bd884a902d220690347
Christopher Auteri <christopher.auteri@icloud.com>
2017-11-20 16:39:44 -0500

    double quotes

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,8 @@ window.onload = function () {
     var to = new Date(2018, 5, 20);
     document.getElementById("current-year").innerHTML = "-"
         + from.getFullYear();
-    document.getElementById("currently").innerHTML = '<small>'
+    document.getElementById("currently").innerHTML = "<small>"
         + Math.floor(Math.round(Math.max((to.getTime() - new Date(from.setHours(0, 0,
                     0, 0)).getTime())
-                / (24 * 60 * 60 * 1000), 0)) / 7) + '</small>';
+                / (24 * 60 * 60 * 1000), 0)) / 7) + "</small>";
 };
