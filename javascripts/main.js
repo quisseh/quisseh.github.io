b8422c2d71a18fa3426dc2f46e4ecca7131660ea
Christopher Auteri <christopher.auteri@icloud.com>
2017-12-23 17:06:44 -0500

    LvR

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,3 @@
 window.onload = function () {
-    var from = new Date();
-    var to = new Date(2018, 5, 20);
-    document.getElementById("current-year").innerHTML = "-"
-        + from.getFullYear();
-    document.getElementById("currently").innerHTML = "<small>"
-        + Math.floor(Math.round(Math.max((to.getTime() - new Date(from.setHours(0, 0,
-                    0, 0)).getTime())
-                / (24 * 60 * 60 * 1000), 0)) / 7) + "</small>";
+    document.getElementById("current-year").innerHTML = "-" + new Date().getFullYear();
 };
